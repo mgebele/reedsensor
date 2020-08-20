2498c300d155f57f8db67f9447737c35e5074b18
markus.gebele <markus.gebele@DEKRADIGITAL.LOCAL>
2020-08-20 17:46:08 +0200

    added reed logic

---
+++ main.py
@@ -0,0 +1,27 @@
+import RPi.GPIO as GPIO
+import time
+import sys
+import signal
+
+# Set Broadcom mode so we can address GPIO pins by number.
+GPIO.setmode(GPIO.BCM)
+
+# Initially we don't know if the door sensor is open or closed...
+isOpen = None
+oldIsOpen = None
+
+# Set up the door sensor pin.
+GPIO.setup(DOOR_SENSOR_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+while True:
+    oldIsOpen = isOpen
+    isOpen = GPIO.input(DOOR_SENSOR_PIN)
+    print(GPIO.input(DOOR_SENSOR_PIN))
+
+    if (isOpen and (isOpen != oldIsOpen)):
+        print("Space is unoccupied!")
+
+    elif (isOpen != oldIsOpen):
+        print("Space is occupied!")
+
+    time.sleep(0.1)
