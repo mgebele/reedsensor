77549225637f224e931522e961f97588c372ab64
markus.gebele <markus.gebele@DEKRADIGITAL.LOCAL>
2020-08-21 14:28:18 +0200

    kk

--- callback.py
+++ callback.py
@@ -4,7 +4,7 @@ import sys
 import signal
 
 
-DOOR_SENSOR_PIN = 18
+DOOR_SENSOR_PIN = [18, 12]
 
 reed_door_open = True
 
@@ -12,29 +12,26 @@ reed_disconnected_time = time.time()
 reed_connected_time = time.time()
 
 
-def my_callback(channel):
+# def my_callback(channel):
 
-    if GPIO.input(DOOR_SENSOR_PIN):     # if port 25 == 1
-        print("FIRST DISconnected")
+#     if GPIO.input(DOOR_SENSOR_PIN):     # if port 25 == 1
+#         print("FIRST DISconnected")
 
-    else:                  # if port 25 != 1
-        print("FIRST connected")
+#     else:                  # if port 25 != 1
+#         print("FIRST connected")
 
 
 # Set Broadcom mode so we can address GPIO pins by number.
 GPIO.setmode(GPIO.BCM)
 
-# This is the GPIO pin number we have one of the door sensor
-# wires attached to, the other should be attached to a ground
-RIGHT_DOOR_SENSOR_PIN = [18, 12]
 
 # Set up the door sensor pins.
-GPIO.setup(RIGHT_DOOR_SENSOR_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(DOOR_SENSOR_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-# add rising edge detection on a channel, ignoring further edges for 200ms for switch bounce handling
-# falling means going from default 1 to 0 (=detected)
-GPIO.add_event_detect(RIGHT_DOOR_SENSOR_PIN, GPIO.BOTH,
-                      callback=my_callback, bouncetime=300)
+# # add rising edge detection on a channel, ignoring further edges for 200ms for switch bounce handling
+# # falling means going from default 1 to 0 (=detected)
+# GPIO.add_event_detect(RIGHT_DOOR_SENSOR_PIN, GPIO.BOTH,
+#                       callback=my_callback, bouncetime=300)
 
 
 def reed_door_ops():
