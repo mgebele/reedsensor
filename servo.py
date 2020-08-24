3361fb4820de840dbc14a43c1173c6ce782bb907
Markus <gebele.markus@googlemail.com>
2020-08-24 23:19:53 +0200

    asda

--- servo.py
+++ servo.py
@@ -3,19 +3,19 @@ import time
 import RPi.GPIO as GPIO
 GPIO.setwarnings(False)
 
+servo = 18
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servo, GPIO.OUT)
+
+
+
 def setServoAngle(servo, angle):
 	pwm = GPIO.PWM(servo, 50)
 	pwm.start(8)
 	dutyCycle = angle / 18. + 3.
 	pwm.ChangeDutyCycle(dutyCycle)
 	time.sleep(0.3)
-
-
-
-servo = 18
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(servo, GPIO.OUT)
-
+	pwm.stop()
 
 try:
   while True:
@@ -23,7 +23,6 @@ try:
     time.sleep(1)
     setServoAngle(servo,0)
 except KeyboardInterrupt:
-  PWM.stop()
   GPIO.cleanup()
 
 
